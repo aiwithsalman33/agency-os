0a598a5ddb80101777e5a1483b61549300e866c3
Aiwithsalman <aiwithsalman33@gmail.com>
2026-08-21 13:00:55 +0530

    Match WhatsApp settings reference UI

--- src/main.tsx
+++ src/main.tsx
@@ -6,5 +6,7 @@ import './extra.css';
 import './settings.css';
 import './crud.css';
 import './client.css';
+import './reference-ui.css';
+import './waba.css';
 
 createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>);
